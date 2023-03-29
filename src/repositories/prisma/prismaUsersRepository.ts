71b64e54536c8c7fe6affcb86724ce59dd416251
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-03-29 17:08:20 -0300

    feat: create prismaUsersRepository and method

---
+++ src/repositories/prisma/prismaUsersRepository.ts
@@ -0,0 +1,33 @@
+import { prisma } from '@/lib/prisma'
+import { Prisma, User } from '@prisma/client'
+import { IUsersRepository } from '../IUsersRepository'
+
+export class PrismaUsersRepository implements IUsersRepository {
+  async create(data: Prisma.UserCreateInput): Promise<User> {
+    const user = await prisma.user.create({
+      data,
+    })
+
+    return user
+  }
+
+  async findById(id: string): Promise<User | null> {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return user
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    return user
+  }
+}
