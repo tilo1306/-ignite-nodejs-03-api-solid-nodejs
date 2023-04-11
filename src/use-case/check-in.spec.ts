f868383a5e4e0930773eb4d6c968171d7d58b6e8
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-11 19:28:31 -0300

    feat:  Implementando casos de uso - Validando distância do check-in

--- src/use-case/check-in.spec.ts
+++ src/use-case/check-in.spec.ts
@@ -81,4 +81,24 @@ describe('Check-in Use Case', () => {
     })
     expect(checkIn.id).toEqual(expect.any(String))
   })
+
+  it('should not be able to check in on distant gym', async () => {
+    gymsRepository.items.push({
+      id: 'gym-02',
+      title: 'Javascript Gym',
+      description: '',
+      phone: '',
+      latitude: new Decimal(100),
+      longitude: new Decimal(100),
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-02',
+        userId: 'user-01',
+        userLatitude: 0,
+        userLongitude: 0,
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
 })
