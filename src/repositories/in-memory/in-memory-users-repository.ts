6850c9c000f61896aa34fd2d642a25a01c0d32d5
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-11 13:27:22 -0300

    test: Design Patterns & Testes - In-Memory Databases

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,38 @@
+import { Prisma, User } from '@prisma/client'
+import { IUsersRepository } from '../IUsersRepository'
+
+export class InMemoryUsersRepository implements IUsersRepository {
+  public items: User[] = []
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = {
+      id: 'user-1',
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+    }
+
+    this.items.push(user as unknown as User)
+
+    return user
+  }
+
+  async findById(id: string): Promise<User | null> {
+    const user = this.items.find((item) => item.id === id)
+
+    if (!user) {
+      return null
+    }
+    return user as User
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = this.items.find((item) => item.email === email)
+
+    if (!user) {
+      return null
+    }
+    return user as User
+  }
+}
