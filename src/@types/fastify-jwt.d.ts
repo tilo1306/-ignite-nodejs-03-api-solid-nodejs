cc5f82e23600132892d1d277dddf21291930a139
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-12 18:54:17 -0300

    feat: Controller de perfil

--- src/@types/fastify-jwt.d.ts
+++ src/@types/fastify-jwt.d.ts
@@ -1,7 +1,7 @@
 import '@fastify/jwt'
 
 declare module '@fastify/jwt' {
-  export interface FastifyJwt {
+  export interface FastifyJWT {
     user: {
       sub: string
     }
--- src/routes/useRoutes.routes.ts
+++ src/routes/useRoutes.routes.ts
@@ -1,8 +1,12 @@
 import { authenticate } from '@/http/controllers/authenticate'
+import { profile } from '@/http/controllers/profile'
 import { register } from '@/http/controllers/register'
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { FastifyInstance } from 'fastify'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/', register)
   app.post('/session', authenticate)
+
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
