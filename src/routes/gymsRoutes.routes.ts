cbd5ea0c97c83acfa01a766e678f04b612109cea
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-12 20:41:45 -0300

    feat: Outros controllers da academia

--- src/routes/gymsRoutes.routes.ts
+++ src/routes/gymsRoutes.routes.ts
@@ -1,6 +1,14 @@
+import { create } from '@/http/controllers/gyms/create'
+import { nearby } from '@/http/controllers/gyms/nearby'
+import { search } from '@/http/controllers/gyms/search'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { FastifyInstance } from 'fastify'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
+
+  app.get('/gyms/search', search)
+  app.get('/gyms/nearby', nearby)
+
+  app.post('/gyms', create)
 }
