bd5e5e256dff45052a2d4e831ec068ac97883ba6
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-12 20:13:31 -0300

    feat: Teste E2E da autenticação

---
+++ src/http/controllers/authenticate.spec.ts
@@ -0,0 +1,31 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to authenticate', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123465',
+    })
+
+    const response = await request(app.server).post('/users/sessions').send({
+      email: 'johndoe@example.com',
+      password: '123465',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
+})
