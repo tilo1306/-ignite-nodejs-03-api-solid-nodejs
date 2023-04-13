411e4cf0f31803f9feb7a94922085bfc0c515b14
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-12 22:40:25 -0300

    feat: Autorização por cargos (RBAC)

--- src/routes/gymsRoutes.routes.ts
+++ src/routes/gymsRoutes.routes.ts
@@ -2,6 +2,7 @@ import { create } from '@/http/controllers/gyms/create'
 import { nearby } from '@/http/controllers/gyms/nearby'
 import { search } from '@/http/controllers/gyms/search'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role'
 import { FastifyInstance } from 'fastify'
 
 export async function gymsRoutes(app: FastifyInstance) {
@@ -10,5 +11,5 @@ export async function gymsRoutes(app: FastifyInstance) {
   app.get('/gyms/search', search)
   app.get('/gyms/nearby', nearby)
 
-  app.post('/gyms', create)
+  app.post('/gyms', { onRequest: [verifyUserRole('ADMIN')] }, create)
 }
